5185ec66d923a714525b15f9fd93f316b709caed
Andrew Kim <akim48415@gmail.com>
2022-06-22 11:25:43 -0400

    init commit, tictactoe Java

---
+++ tictactoeJava/src/Board.java
@@ -0,0 +1,11 @@
+public class Board {
+    private static int row = 3;
+    private static int col = 3;
+
+    //Piece [][]ourBoard = new Piece[row][col];
+
+    public Board() {
+        System.out.println("Sup");
+        return;
+    }
+}
